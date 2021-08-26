16d19b1d342bbb9888ad4afca2333d4a75e5dc2a
Sebastian <sejostrand@gmail.com>
2021-08-26 20:56:13 +0100

    footer

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Hero from '../components/Hero/Hero';
 export default function Home() {
   return (
     <div className={styles.pageContainer}>
-      <div className={styles.gradientBar}></div>
+      {/* <div className={styles.gradientBar}></div> */}
       <Navbar />
       <Hero />
       <div className={styles.container}></div>
