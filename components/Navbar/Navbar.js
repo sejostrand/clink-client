88d71e65b7bd3a9f41f89835f5269dec7e28f3e1
Sebastian <sejostrand@gmail.com>
2021-08-26 15:22:07 +0100

    hero added

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -4,7 +4,9 @@ import MobileNav from './MobileNav';
 import useMediaQuery from '../../utils/useMediaQuery';
 
 const Navbar = (props) => {
-  const isMobile = useMediaQuery('(max-width: 620px)');
+  //const isMobile = useMediaQuery('(max-width: 620px)');
+  const isMobile = false;
+
   return isMobile ? (
     <MobileNav refresh={props.refresh} setRefresh={props.setRefresh} />
   ) : (
